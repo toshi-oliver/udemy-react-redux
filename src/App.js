d418a584875a9de3bd96618b1f39041f17ab11b0
Toshiaki Furukawa <furukawa@serverworks.co.jp>
2020-06-19 07:33:13 +0900

    my own boiler plate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { render } from "@testing-library/react";
 
 function App() {
+  render();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello world!</h1>
     </div>
   );
 }
