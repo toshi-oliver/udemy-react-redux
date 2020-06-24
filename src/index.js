31dc4388fce6aabeb7b587d87c19efd7f9107519
Toshiaki Furukawa <furukawa@serverworks.co.jp>
2020-06-25 07:21:00 +0900

    apply material-ui to eventsIndex

--- src/index.js
+++ src/index.js
@@ -11,23 +11,27 @@ import EventsIndex from "./components/events_index";
 import EventsNew from "./components/events_new";
 import EventsShow from "./components/events_show";
 import * as serviceWorker from "./serviceWorker";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 const enhancer =
   process.env.NODE_ENV === "development"
     ? composeWithDevTools(applyMiddleware(thunk))
     : applyMiddleware(thunk);
 const store = createStore(reducer, enhancer);
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/events/new" component={EventsNew} />
-        <Route path="/events/:id" component={EventsShow} />
-        <Route exact path="/" component={EventsIndex} />
-        <Route exact path="/events/" component={EventsIndex} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/events/new" component={EventsNew} />
+          <Route path="/events/:id" component={EventsShow} />
+          <Route exact path="/" component={EventsIndex} />
+          <Route exact path="/events/" component={EventsIndex} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById("root")
 );
 
